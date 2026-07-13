0e000355d6a0d4c1db90a429203309408a683a4a
Naineshramani <neelkanthvarnienterprises@gmail.com>
2026-07-13 15:35:53 -0400

    Fix fatal error on first email verification resend
    VerificationController used the "Illuminate\Foundation\Auth\VerifiesEmails"
    trait, which was removed from Laravel core well before the app's pinned
    laravel/framework ^12.0. The only piece of it this controller actually
    relied on was redirectPath(), so that's now defined directly instead of
    importing the missing trait.

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use Illuminate\Foundation\Auth\VerifiesEmails;
 use App\Http\Controllers\OTPVerificationController;
 use Carbon\Carbon;
 use App\Models\User;
@@ -22,14 +21,20 @@ class VerificationController extends Controller
     |
     */
 
-    use VerifiesEmails;
-
     /**
      * Where to redirect users after verification.
      *
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Where to redirect the user after verification.
+     */
+    protected function redirectPath()
+    {
+        return $this->redirectTo;
+    }
 
     /**
      * Create a new controller instance.
